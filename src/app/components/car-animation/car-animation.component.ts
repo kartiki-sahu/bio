d20ce81c6bdf2ef708b1c5c5ebe7fa9dbb0de2c8
Kartiki.Sahu <kartiki.sahu@globallogic.com>
2020-09-06 15:49:01 +0530

    car animation completed

--- src/app/components/car-animation/car-animation.component.ts
+++ src/app/components/car-animation/car-animation.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery';
 @Component({
   selector: 'app-car-animation',
   templateUrl: './car-animation.component.html',
@@ -10,6 +10,22 @@ export class CarAnimationComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $(window).scroll(function() {
+      const highwayPos = $('.highway').offset().top;
+      const cityPos = $('.city').offset().top;
+      const topOfWindow = $(window).scrollTop();
+      if (highwayPos < topOfWindow + 800) {
+        $('.highway').addClass('highwayAnimation');
+      } else {
+        $('.highway').removeClass('highwayAnimation');
+      }
+
+      if (cityPos < topOfWindow + 700) {
+        $('.city').addClass('cityAnimation');
+      } else {
+        $('.city').removeClass('cityAnimation');
+      }
+      });
   }
 
 }
