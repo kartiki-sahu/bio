ade282952cb1c71f1f2d9d0ad9ca0737726b2684
Kartiki.Sahu <kartiki.sahu@globallogic.com>
2021-02-09 22:49:14 +0530

    resolved header navigation issue

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,23 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule, } from '@angular/common';
-import { BrowserModule  } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { ComponentsComponent } from './components/components.component';
 
 const routes: Routes = [
-    { path: '', component: ComponentsComponent }
+  { path: '', component: ComponentsComponent }
 ];
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserModule,
-    RouterModule.forRoot(routes,{
-      preloadingStrategy: PreloadAllModules
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules,
+      scrollPositionRestoration: 'enabled',
+      anchorScrolling: 'enabled',
+      scrollOffset: [0, 60]
     })
   ],
-  exports: [
-  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
